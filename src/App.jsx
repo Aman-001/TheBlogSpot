5d639e76298899988c5e44f0707897f47433ae94
Aman Singh Chauhan <amanc228d@gmail.com>
2025-07-08 17:39:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authservice from "./appwrite/auth.js"
+import {login , logout} from "./store/authSlice.js"
+import './App.css'
+import { Header, Footer } from  "../src/components/index.js"
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  const [loading, setLoading] = useState(true)
+  const dispach = useDispatch()
+
+  useEffect(() => {
+    authservice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispach(login(userData))
+        } else {
+          dispach(logout())
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+        dispach(logout())
+      })
+      .finally(() => {
+        setLoading(false)
+      }
+  )}, [])
+
+
+return !loading ? (
+  <div className="min-h-screen flex flex-col">
+    <Header />
+    
+    <main className="flex-grow">
+      <Outlet />
+    </main>
+
+    <Footer />
+  </div>
+) : null;
+ 
+}
+
+export default App
